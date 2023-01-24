2baaceb56a514d1c0caf817b039e1b1d0cb47410
vojajokic <55879745+vojajokic@users.noreply.github.com>
2023-01-24 14:56:29 +0000

    Model binding from Qurey String (2.3)

---
+++ src/BigStar/Pages/Binding/ToQueryString.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BigStar.Pages.Binding
+{
+  [BindProperties(SupportsGet = true)]
+  public class ToQueryStringModel : PageModel
+  {
+    public string CardName { get; set; }
+    public decimal? OriginalPrice { get; set; }
+    public void OnGet()
+    {
+        if (string.IsNullOrWhiteSpace(CardName))
+        {
+            CardName="No card name found!";
+        }
+    }
+  }
+}
--- src/BigStar/Pages/ShowDates.cshtml.cs
+++ src/BigStar/Pages/ShowDates.cshtml.cs
@@ -3,10 +3,29 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MyApp.Namespace
 {
-    public class ShowDatesModel : PageModel
+  public class ShowDatesModel : PageModel
+  {
+    public IEnumerable<DateOnly> SaleDates { get; set; }
+    public void OnGet()
     {
-        public void OnGet()
-        {
-        }
+      SaleDates = GetSaleDates(DateOnly.FromDateTime(DateTime.Now));
     }
+    private IEnumerable<DateOnly> GetSaleDates(DateOnly startingDate)
+    {
+      var allDays = AllDatesInMonth(startingDate.Year, startingDate.Month);
+      var SaleDays = allDays.Where(x => x.DayOfWeek == DayOfWeek.Monday
+                                    || x.DayOfWeek == DayOfWeek.Wednesday);
+      return SaleDays;
+
+    }
+    public IEnumerable<DateOnly> AllDatesInMonth(int year, int month)
+    {
+      int days = DateTime.DaysInMonth(year, month);
+      for (int day = 1; day <= days; day++)
+      {
+        yield return new DateOnly(year, month, day);
+      }
+
+    }
+  }
 }
